90a445f2092513e3294a93212a9ff918622f4cad
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-10-15 19:24:04 +0200

    feat: add dockerfile and dockerignore

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,13 +2,11 @@ import 'dotenv/config';
 import * as joi from 'joi';
 
 interface EnvVars {
-  PORT: number;
   NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
   .object({
-    PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string().required()).required(),
   })
   .unknown(true);
@@ -25,6 +23,5 @@ if (error) {
 const envVars: EnvVars = value;
 
 export const envs = {
-  port: envVars.PORT,
   natsServers: envVars.NATS_SERVERS,
 };
