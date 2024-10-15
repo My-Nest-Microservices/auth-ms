4b0c7a4d535cd37fcc1cfa19010acb22b6980510
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-10-15 19:10:39 +0200

    refactor: from http server to microservice

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
-  logger.log(`Application is running on: ${envs.port}`);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen();
+
+  logger.log(`Auth microservice is running`);
 }
 bootstrap();
