f7524ab8944fcc3fd0ccaa45dcdfef578f1609b6
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-10-21 10:52:50 +0200

    feat: register and login user dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,17 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @MessagePattern('auth.register.user')
-  async registerUser(@Payload() user: any) {
-    console.log({ user });
+  async registerUser(@Payload() user: RegisterUserDto) {
     return user;
   }
   @MessagePattern('auth.login.user')
-  async loginUser(@Payload() user: any) {
+  async loginUser(@Payload() user: LoginUserDto) {
     return user;
   }
   @MessagePattern('auth.verify.user')
